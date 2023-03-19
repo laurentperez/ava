63c561909d65804e242c4a12c429e9bdb053367d
Laurent Perez <laurent.perez@itk.fr>
2023-03-19 12:33:41 +0100

    fix doc. prepare for llama. document api.

--- ava/src/main/kotlin/fr/ava/ia/service/hf/bloom/BloomResource.kt
+++ ava/src/main/kotlin/fr/ava/ia/service/hf/bloom/BloomResource.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.inject.Inject
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
+import org.eclipse.microprofile.openapi.annotations.tags.Tag
 
 @Path("/hf/bloom")
 class BloomResource {
@@ -11,6 +12,7 @@ class BloomResource {
     @Inject
     private lateinit var bloomService: BloomService
 
+    @Tag(name = "Bloom prompt", description = "The prompt")
     @POST
     fun prompt(prompt: BloomPrompt): String {
         return bloomService.prompt(prompt.msg)
