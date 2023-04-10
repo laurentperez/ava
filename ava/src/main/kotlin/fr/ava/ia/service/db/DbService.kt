30366178ff4ed16b9b2c58bb63b4fe91afd24c84
Laurent Perez <laurent.perez@itk.fr>
2023-04-10 16:10:52 +0200

    no private for quarkus reflection. drop find()s. add user crud

--- ava/src/main/kotlin/fr/ava/ia/service/db/DbService.kt
+++ ava/src/main/kotlin/fr/ava/ia/service/db/DbService.kt
@@ -1,7 +1,9 @@
 package fr.ava.ia.service.db
 
+import fr.ava.ia.model.ChatPartner
 import fr.ava.ia.model.Conversation
 import fr.ava.ia.model.LastExchange
+import fr.ava.ia.repository.ChatPartnerRepository
 import fr.ava.ia.repository.ConversationRepository
 import fr.ava.ia.repository.LastExchangeRepository
 import io.quarkus.panache.common.Sort
@@ -13,13 +15,16 @@ import org.jboss.logging.Logger
 @ApplicationScoped
 class DbService {
 
-    @Inject
-    private lateinit var conversationRepository: ConversationRepository
-
-    @Inject
-    private lateinit var lastExchangeRepository: LastExchangeRepository
-
     private var logger = Logger.getLogger(this::class.java.name)
+
+    @Inject
+    lateinit var conversationRepository: ConversationRepository
+
+    @Inject
+    lateinit var lastExchangeRepository: LastExchangeRepository
+
+    @Inject
+    lateinit var chatPartnerRepository: ChatPartnerRepository
 
     @Transactional
     fun saveConversation(conversation: Conversation) {
@@ -28,14 +33,13 @@ class DbService {
     }
 
     @Transactional
-    fun findConversationWithUser(userID : String) : List<Conversation> {
+    fun findConversationWithUserWithRootId(userID : String, rootID: String) : List<Conversation> {
         // return conversationRepository.list("from Conversation where userID=?1", userID)
-        return conversationRepository.find("#Conversation.getByUserID", userID).list()
-
+        return conversationRepository.find("#Conversation.withUserWithRootId", userID, rootID).list()
     }
     @Transactional
     fun findAllConversations() : List<Conversation> {
-        return conversationRepository.findAll().list()
+        return conversationRepository.listAll()
     }
 
     @Transactional
@@ -48,5 +52,20 @@ class DbService {
     fun findLastExchange() : LastExchange {
         return lastExchangeRepository.findAll(Sort.by("id").descending()).firstResult()
         // return lastExchangeRepository.findAll(Sort.descending()).firstResult()
+    }
+
+    @Transactional
+    fun findAllUsers() : List<ChatPartner> {
+        return chatPartnerRepository.listAll()
+    }
+
+    @Transactional
+    fun findUserById(userId: String) : ChatPartner {
+        return chatPartnerRepository.find("userId", userId).singleResult()
+    }
+
+    @Transactional
+    fun saveUser(userId: String, userName: String) {
+        chatPartnerRepository.persistAndFlush(ChatPartner(userId, userName))
     }
 }
