78748035ef9c784543d28f9548ad799f9db561f5
Laurent Perez <laurent.perez@itk.fr>
2023-04-10 16:11:25 +0200

    fix !enabled bot

--- ava/src/main/kotlin/fr/ava/ia/service/messaging/MattermostClientProducer.kt
+++ ava/src/main/kotlin/fr/ava/ia/service/messaging/MattermostClientProducer.kt
@@ -1,11 +1,10 @@
 package fr.ava.ia.service.messaging
 
+import io.quarkus.runtime.configuration.ConfigUtils
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.inject.Produces
-import jakarta.ws.rs.client.ClientBuilder
 import net.bis5.mattermost.client4.MattermostClient
 import org.eclipse.microprofile.config.inject.ConfigProperty
-import java.util.concurrent.TimeUnit
 
 @ApplicationScoped
 class MattermostClientProducer(
@@ -27,8 +26,11 @@ class MattermostClientProducer(
             //.httpConfig { it.connectTimeout(1, TimeUnit.SECONDS) }
             .build()
         var serverOk = false
-        if(enabled) serverOk = !mmClient.login(login, password).checkStatusOk().hasError()
-        if(!serverOk) throw IllegalStateException("chat server is not ready ! check bot login status")
+        if(enabled) {
+            serverOk = mmClient.login(login, password).rawResponse.status == 200
+        }
+        val p = ConfigUtils.getProfiles() // avoid %test profile throw
+        if(!serverOk && !p.contains("test")) throw IllegalStateException("chat server is not ready ! check bot login status")
         return mmClient
     }
 }
